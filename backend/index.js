dc6df4c7fffd231e65b92592e0fdfd74daf55d5a
PUNEETH814 <princepuneeths814@gmail.com>
2024-08-25 23:09:41 +0530

    port

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const Post = require("./models/Post"); // Adjust the path as necessary
 //middlewares
 dotenv.config();
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.PORT, credentials: true }));
 app.use(cookieParser());
 
 //routes middleware
